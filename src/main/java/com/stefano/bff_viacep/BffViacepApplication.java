19a8ac1cd611aaadc747c55e7196c6189d765689
Stefano Sarmento <stefano_2007@hotmail.com>
2026-04-03 15:12:51 -0300

    iniciando projeto

---
+++ src/main/java/com/stefano/bff_viacep/BffViacepApplication.java
@@ -0,0 +1,13 @@
+package com.stefano.bff_viacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffViacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffViacepApplication.class, args);
+	}
+
+}
